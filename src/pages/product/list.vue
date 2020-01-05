d2b59b14c881220d2516fff9ebf2e08d43d61590
MrZhang3512 <15235233600@sohu.com>
2020-01-05 21:40:48 +0800

    图片上传

--- src/pages/product/list.vue
+++ src/pages/product/list.vue
@@ -12,6 +12,7 @@
         <el-table-column label="价格" prop="price"></el-table-column>
         <el-table-column label="描述" prop="description"></el-table-column>
         <el-table-column label="所属产品" prop="categoryId"></el-table-column>
+         <el-table-column label="照片" prop="photo" width="650px"></el-table-column>
         <el-table-column label="操作" >
              <template v-slot="slot">
                     <a href="" @click.prevent="todeleteHandler(slot.row.id)">删除</a>
@@ -43,7 +44,7 @@
                  <el-input v-model="form.price"></el-input>
              </el-form-item>
              <el-form-item label="描述">
-                 <el-input v-model="form.description"></el-input>
+                 <el-input v-model="form.description" type="textarea"></el-input>
              </el-form-item>
              <el-form-item label="所属栏目">
                   <template>
@@ -55,7 +56,19 @@
                 :value="item.id">
                 </el-option>
             </el-select>
+            
             </template>
+             </el-form-item>
+             <el-form-item label="照片">
+                <el-upload
+                    class="upload-demo"
+                    action="https://134.175.154.93:6677/file/upload"
+                    :file-list="fileList"
+                    :on-success="uploadSuccessHandler"
+                    list-type="picture">
+                    <el-button size="small" type="primary">点击上传</el-button>
+                    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                    </el-upload>
              </el-form-item>
          </el-form>
          {{form}}
@@ -76,7 +89,8 @@ export default {
             products:[],
             visible:false,
             form:{},
-            options: []
+            options: [],
+            fileList:[]
         }
     },
     created(){
@@ -84,7 +98,13 @@ export default {
         this.loadCategory();
     },
     methods:{
+        uploadSuccessHandler(response){
+            let photo="http://134.175.154.93:8888/group1/"+response.id;
+            //将图片地址设置到form中,提交后台
+            this.form.photo=photo;
+        },
         toupdateHandler(row){
+            this.fileList=[];
             this.form=row;
             this.visible=true;
         },
@@ -122,6 +142,7 @@ export default {
             this.loadData();})
         },
         toAddHandler(){
+            this.fileList=[];
             this.visible=true;
         },
         todeleteHandler(id){
